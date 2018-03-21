258a711ef81a49a6f5a959ad67af52a25da516ce
varunnandu1992 <varunnandu1992@ccs.neu.edu>
2018-03-21 18:42:50 -0400

    ok

--- plag-detector/src/main/java/edu/northeastern/cs5500/Dao/SemesterDao.java
+++ plag-detector/src/main/java/edu/northeastern/cs5500/Dao/SemesterDao.java
@@ -1,7 +1,6 @@
 package edu.northeastern.cs5500.Dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
