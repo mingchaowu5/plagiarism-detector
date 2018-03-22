378afc8be36a4d88c6bfa766418ee1480c14d367
varunnandu1992 <varunnandu1992@ccs.neu.edu>
2018-03-22 00:56:56 -0400

    Chal ja bhai

--- plag-detector/src/main/java/edu/northeastern/cs5500/Dao/CourseDao.java
+++ plag-detector/src/main/java/edu/northeastern/cs5500/Dao/CourseDao.java
@@ -15,38 +15,36 @@ import edu.northeastern.cs5500.models.Course.Course;
 @Repository
 
 public class CourseDao {
-	
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	public List<Course> findAllCourses(){
+
+	public List<Course> findAllCourses() {
 		String sql = "Select * from Courses";
 		RowMapper<Course> rowMapper = new BeanPropertyRowMapper<>(Course.class);
 		List<Course> results = this.jdbcTemplate.query(sql, rowMapper);
 		return results;
 	}
-	
-	public List<Course> findAllCoursesForSemester(int id){
+
+	public List<Course> findAllCoursesForSemester(int id) {
 		String sql = "Select * from Courses WHERE Courses.semester = ?";
 		RowMapper<Course> rowMapper = new BeanPropertyRowMapper<>(Course.class);
 		List<Course> results = this.jdbcTemplate.query(sql, rowMapper, id);
 		return results;
 	}
-	
-	public List<Course> findAllCoursesForProfessor(int id){
+
+	public List<Course> findAllCoursesForProfessor(int id) {
 		String sql = "Select Courses.* from Courses join ProfessorCourseMapping on Courses.id = ProfessorCourseMapping.course WHERE ProfessorCourseMapping.professor = ?";
 		RowMapper<Course> rowMapper = new BeanPropertyRowMapper<>(Course.class);
 		List<Course> results = this.jdbcTemplate.query(sql, rowMapper, id);
 		return results;
 	}
-	
-	public List<Course> findAllCoursesForStudent(int studentId){
+
+	public List<Course> findAllCoursesForStudent(int studentId) {
 		String sql = "Select Courses.* from Courses join StudentCourseMapping on Courses.id = StudentCourseMapping.course WHERE StudentCourseMapping.student = ?";
 		RowMapper<Course> rowMapper = new BeanPropertyRowMapper<>(Course.class);
 		List<Course> results = this.jdbcTemplate.query(sql, rowMapper, studentId);
 		return results;
 	}
-	
 
 }
