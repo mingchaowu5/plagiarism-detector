dbcf0c12ebb9b5592ce5690aeefd11c4c0002586
tachyon <mittal.kr@husky.neu.edu>
2018-03-22 16:05:25 -0400

    CS-14 #comment #close

--- plag-detector/src/main/java/edu/northeastern/cs5500/controller/file/FileController.java
+++ plag-detector/src/main/java/edu/northeastern/cs5500/controller/file/FileController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class FileController {
 	 * Get all the courses when an user logs in.
 	 * @return
 	 */
-	@GetMapping(value = "/uploadFile")
+	@PostMapping(value = "/uploadFile")
 	public ResponseEntity<File> uploadFile(@RequestPart(value = "file") MultipartFile multiPartFile, 
 			@RequestPart(value = "s_id") int student_id, @RequestPart(value = "a_id") int assignment_id) {
 		File file = null;
